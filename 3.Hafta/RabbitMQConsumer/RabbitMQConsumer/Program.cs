ac5882e8dc75fa71ea07f2e7607975f9d6596b94
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-08-05 15:19:23 +0300

    added rabbitmq projects

---
+++ 3.Hafta/RabbitMQConsumer/RabbitMQConsumer/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+using RabbitMQConsumer;
+using Newtonsoft.Json;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest"
+};
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    //bağlanacağımız kuyruğun adını belirtiyoruz 
+    channel.QueueDeclare(queue: "btkakademiq",
+                       durable: false,
+                       exclusive: false,
+                       autoDelete: false,
+                       arguments: null);
+    //Event Delegate 
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        //kuyruktan okumaya başlayacak
+        var body = ea.Body.ToArray();
+        //Byte array olrarak kuyruktan okuduğu veriyi önce String e dönüştürecek
+
+        var message = Encoding.UTF8.GetString(body);
+        Product p1 = new Product();
+        //String olarak dönüştürdüğü veriye DeSerialezi ile tekra produc objesine dönüşütürecek
+        p1 = JsonConvert.DeserializeObject<Product>(message);
+        Console.WriteLine(" [x] Alınan mesaj {0}", p1.Name);
+        Console.WriteLine(" Alınan {0}", message);
+
+    };
+
+    channel.BasicConsume(queue: "btkakademiq",
+                     autoAck: true,
+                     consumer: consumer);
+
+    Console.WriteLine(" Çıkmak için bi tuşa basın");
+    Console.ReadLine();
+}
