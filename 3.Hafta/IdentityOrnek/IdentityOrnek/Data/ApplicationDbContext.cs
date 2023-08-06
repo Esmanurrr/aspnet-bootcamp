d1b4ba552398f0d3b19ccea62f0d5c268f521b6d
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-08-06 11:23:26 +0300

    added register and login forms

---
+++ 3.Hafta/IdentityOrnek/IdentityOrnek/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using IdentityOrnek.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityOrnek.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<CustomUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
